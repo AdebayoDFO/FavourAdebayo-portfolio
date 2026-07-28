c967433c1bc1009db1900556926792b2247baa68
AdebayoDFO <divinefavouradebayo2008@gmail.com>
2026-07-28 17:41:53 +0100

    Remove old files and update config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://favouradebayo-portflio.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'AdebayoDFO', // Usually your GitHub org/user name.
+  projectName: 'FavourAdebayo-Portfolio', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
